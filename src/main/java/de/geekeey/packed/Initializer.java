d575e7ced06529d5c3a7e808f05f9deffacaf95f
Louis Seubert <louis.seubert.ls@gmail.com>
2020-08-02 19:50:49 +0200

    init

---
+++ src/main/java/de/geekeey/packed/Initializer.java
@@ -0,0 +1,11 @@
+package de.geekeey.packed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Initializer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/geekeey/packed/client/ClientInitializer.java
@@ -0,0 +1,14 @@
+package de.geekeey.packed.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ClientInitializer implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
