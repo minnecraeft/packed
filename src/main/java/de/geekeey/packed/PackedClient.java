bc2fb3461e1fadddfc016fff32a0840c6a8f64cf
Jan Thomas <jan.thomas.118@gmail.com>
2020-08-13 18:21:26 +0200

    Fix broken merge

--- src/main/java/de/geekeey/packed/PackedClient.java
+++ src/main/java/de/geekeey/packed/PackedClient.java
@@ -8,10 +8,7 @@ import de.geekeey.packed.client.render.StorageBarrelEntityRenderer;
 import de.geekeey.packed.init.PackedEntities;
 import de.geekeey.packed.init.PackedItems;
 import de.geekeey.packed.init.PackedScreenHandlers;
-import de.geekeey.packed.init.helpers.ChestTier;
-import de.geekeey.packed.init.helpers.ChestTiers;
-import de.geekeey.packed.init.helpers.WoodItemVariants;
-import de.geekeey.packed.init.helpers.WoodVariants;
+import de.geekeey.packed.init.helpers.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -23,6 +20,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+
+import java.util.function.Function;
 
 @Environment(EnvType.CLIENT)
 public class PackedClient implements ClientModInitializer {
@@ -40,35 +39,29 @@ public class PackedClient implements ClientModInitializer {
 
         //Registration of lock textures
         for (ChestTiers value : ChestTiers.values()) {
-            ClientSpriteRegistryCallback.event(TexturedRenderLayers.CHEST_ATLAS_TEXTURE).register((atlasTexture, registry) -> registry.register(Packed.id("entity/chest/" + value.identifier())));
+            ClientSpriteRegistryCallback.event(TexturedRenderLayers.CHEST_ATLAS_TEXTURE).register((atlasTexture, registry) -> registry.register(Packed.id("entity/chest/" + value.identifier().getPath())));
         }
 
-        // register entity type renderer and second register item rendere to use this entity type renderer internally
-        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CHEST_3_9, PackedClient::createDefaultRenderer);
-        register(PackedItems.CHEST_DEFAULT_TIER, CustomChestEntity.create3x9());
-
-        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CHEST_4_9, PackedClient::createDefaultRenderer);
-        register(PackedItems.CHEST_TIER_1, CustomChestEntity.create4x9());
-
-        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CHEST_5_9, PackedClient::createDefaultRenderer);
-        register(PackedItems.CHEST_TIER_2, CustomChestEntity.create5x9());
-
-        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CHEST_6_9, PackedClient::createDefaultRenderer);
-        register(PackedItems.CHEST_TIER_3, CustomChestEntity.create6x9());
+        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CUSTOM_CHEST, PackedClient::createDefaultRenderer);
+        register(PackedItems.CHEST_DEFAULT_TIER, variant -> new CustomChestEntity(ChestTiers.DEFAULT, variant));
+        register(PackedItems.CHEST_TIER_1, variant -> new CustomChestEntity(ChestTiers.TIER_1, variant));
+        register(PackedItems.CHEST_TIER_2, variant -> new CustomChestEntity(ChestTiers.TIER_2, variant));
+        register(PackedItems.CHEST_TIER_3, variant -> new CustomChestEntity(ChestTiers.TIER_3, variant));
 
         BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.STORAGE_BARREL_ENTITY, StorageBarrelEntityRenderer::new);
+
     }
 
     private static CustomChestEntityRenderer createDefaultRenderer(BlockEntityRenderDispatcher dispatcher) {
         return new CustomChestEntityRenderer(dispatcher, Blocks.CHEST);
     }
 
-    private static void register(WoodItemVariants<ChestTier, CustomChest> items, CustomChestEntity entity) {
-        items.forEach(item -> {
+    private static void register(WoodItemVariants<ChestTier, CustomChest> items, Function<WoodVariant, CustomChestEntity> factory) {
+        items.variants.forEach((variant, item) -> {
             Block block = item.getBlock();
             CustomChestEntityRenderer renderer = new CustomChestEntityRenderer(BlockEntityRenderDispatcher.INSTANCE, block);
             BuiltinItemRendererRegistry.INSTANCE.register(item, (stack, matrices, vertices, light, overlay) -> {
-                renderer.render(entity, 0.0f, matrices, vertices, light, overlay);
+                renderer.render(factory.apply(variant), 0.0f, matrices, vertices, light, overlay);
             });
         });
     }
