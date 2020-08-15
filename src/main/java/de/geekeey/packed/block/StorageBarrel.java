131008691f7e4b24516a092e84da1ad87773602c
Jan Thomas <jan.thomas.118@gmail.com>
2020-08-15 16:10:34 +0200

    Made StorageBarrel not drop Items when upgraded

--- src/main/java/de/geekeey/packed/block/StorageBarrel.java
+++ src/main/java/de/geekeey/packed/block/StorageBarrel.java
@@ -168,14 +168,13 @@ public class StorageBarrel extends BlockWithEntity {
      **/
     @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
-        if (!state.isOf(newState.getBlock())) {
+        //Do not items when new state is a storage barrel, this would mean the barrel was upgraded!
+        if (!state.isOf(newState.getBlock()) && !(newState.getBlock() instanceof StorageBarrel)) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
             if (blockEntity instanceof Inventory) {
                 ItemScatterer.spawn(world, pos, (Inventory) blockEntity);
                 world.updateComparators(pos, this);
             }
-
-            super.onStateReplaced(state, world, pos, newState, moved);
         }
     }
 
