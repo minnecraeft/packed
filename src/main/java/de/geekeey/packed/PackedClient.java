a1dedd3a1089a259251ce307b06f4a33c0c35979
Louis Seubert <louis.seubert.ls@gmail.com>
2020-08-21 22:06:55 +0200

    fixed crash when piston is moving

--- src/main/java/de/geekeey/packed/PackedClient.java
+++ src/main/java/de/geekeey/packed/PackedClient.java
@@ -14,10 +14,12 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 
@@ -42,11 +44,14 @@ public class PackedClient implements ClientModInitializer {
             ClientSpriteRegistryCallback.event(TexturedRenderLayers.CHEST_ATLAS_TEXTURE).register((atlasTexture, registry) -> registry.register(Packed.id("entity/chest/" + value.getIdentifier().getPath())));
         }
 
-        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CUSTOM_CHEST, PackedClient::createDefaultRenderer);
-        register(PackedItems.CHEST_DEFAULT, variant -> new VariantChestBlockEntity(StorageTiers.DEFAULT, variant));
-        register(PackedItems.CHEST_TIER_1, variant -> new VariantChestBlockEntity(StorageTiers.TIER_1, variant));
-        register(PackedItems.CHEST_TIER_2, variant -> new VariantChestBlockEntity(StorageTiers.TIER_2, variant));
-        register(PackedItems.CHEST_TIER_3, variant -> new VariantChestBlockEntity(StorageTiers.TIER_3, variant));
+        BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CUSTOM_CHEST, dispatcher -> {
+            register(PackedItems.CHEST_DEFAULT, dispatcher, variant -> new VariantChestBlockEntity(StorageTiers.DEFAULT, variant));
+            register(PackedItems.CHEST_TIER_1, dispatcher, variant -> new VariantChestBlockEntity(StorageTiers.TIER_1, variant));
+            register(PackedItems.CHEST_TIER_2, dispatcher, variant -> new VariantChestBlockEntity(StorageTiers.TIER_2, variant));
+            register(PackedItems.CHEST_TIER_3, dispatcher, variant -> new VariantChestBlockEntity(StorageTiers.TIER_3, variant));
+            return createDefaultRenderer(dispatcher);
+        });
+
 
         BlockEntityRendererRegistry.INSTANCE.register(PackedEntities.CRATE, VariantCrateBlockEntityRenderer::new);
 
@@ -56,10 +61,11 @@ public class PackedClient implements ClientModInitializer {
         return new VariantChestBlockEntityRenderer(dispatcher, Blocks.CHEST);
     }
 
-    private static void register(WoodItemVariants<StorageTier, VariantChestBlock> items, Function<WoodVariant, VariantChestBlockEntity> factory) {
+    private static void register(WoodItemVariants<StorageTier, VariantChestBlock> items, BlockEntityRenderDispatcher dispatcher, Function<WoodVariant, VariantChestBlockEntity> factory) {
         items.variants.forEach((variant, item) -> {
             Block block = item.getBlock();
-            VariantChestBlockEntityRenderer renderer = new VariantChestBlockEntityRenderer(BlockEntityRenderDispatcher.INSTANCE, block);
+
+            VariantChestBlockEntityRenderer renderer = new VariantChestBlockEntityRenderer(dispatcher, block);
             BuiltinItemRendererRegistry.INSTANCE.register(item, (stack, matrices, vertices, light, overlay) -> {
                 renderer.render(factory.apply(variant), 0.0f, matrices, vertices, light, overlay);
             });
