b4abffd87e82b92557086267f591d2fec808ebbc
Louis Seubert <louis.seubert.ls@gmail.com>
2020-08-21 09:07:26 +0200

    some script changes as well as json cleanup
    let's hope we don't break something this time...

--- scripts/chest.sh
+++ scripts/chest.sh
@@ -2,6 +2,11 @@
 
 BASE="$(dirname "$(readlink -fn "$0")")"
 
+if [[ -z $ASSETS || -z $DATA ]]; then
+  printf "The environment variable 'ASSETS' or 'DATA' is not set"
+  exit 1
+fi
+
 BLOCK_STATES="${ASSETS}/blockstates"
 MODELS_BLOCK="${ASSETS}/models/block"
 MODELS_ITEM="${ASSETS}/models/item"
@@ -16,25 +21,30 @@ RECIPE_ADVANCEMENTS="${DATA}/advancements/recipes"
 [[ ! -d "${RECIPES}" ]] && mkdir -p "${RECIPES}"
 [[ ! -d "${RECIPE_ADVANCEMENTS}" ]] && mkdir -p "${RECIPE_ADVANCEMENTS}"
 
+STORAGE_TYPE='chest'
 VARIANTS=('acacia' 'birch' 'dark_oak' 'jungle' 'oak' 'spruce' 'crimson' 'warped')
 TIERS=('default' 'tier1' 'tier2' 'tier3')
 
 for VARIANT in "${VARIANTS[@]}"; do
+  printf " +---> json for %s with variant '%-8s' is generating\n" "$STORAGE_TYPE" "$VARIANT"
+
   export VARIANT
   for TIER in "${TIERS[@]}"; do
     export TIER
     export PREVIOUS_TIER
-    file="${VARIANT}_chest_${TIER}"
-    envsubst <"${BASE}/presets/chest/blockstate.json" >"${BLOCK_STATES}/${file}.json"
 
-    envsubst <"${BASE}/presets/chest/model_block.json" >"${MODELS_BLOCK}/${file}.json"
-    envsubst <"${BASE}/presets/chest/model_item.json" >"${MODELS_ITEM}/${file}.json"
+    file="${VARIANT}_${STORAGE_TYPE}_${TIER}"
+
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/blockstate.json" >"${BLOCK_STATES}/${file}.json"
+
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/model_block.json" >"${MODELS_BLOCK}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/model_item.json" >"${MODELS_ITEM}/${file}.json"
 
     [[ ! -d "${RECIPES}/${VARIANT}" ]] && mkdir -p "${RECIPES}/${VARIANT}"
-    envsubst <"${BASE}/presets/chest/recipe_${TIER}.json" >"${RECIPES}/${VARIANT}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/recipe_${TIER}.json" >"${RECIPES}/${VARIANT}/${file}.json"
 
     [[ ! -d "${RECIPE_ADVANCEMENTS}/${VARIANT}" ]] && mkdir -p "${RECIPE_ADVANCEMENTS}/${VARIANT}"
-    envsubst < "${BASE}/presets/chest/advancement_${TIER}.json" > "${RECIPE_ADVANCEMENTS}/${VARIANT}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/advancement_${TIER}.json" >"${RECIPE_ADVANCEMENTS}/${VARIANT}/${file}.json"
 
     PREVIOUS_TIER="${TIER}"
   done
--- scripts/crate.sh
+++ scripts/crate.sh
@@ -2,6 +2,11 @@
 
 BASE="$(dirname "$(readlink -fn "$0")")"
 
+if [[ -z $ASSETS || -z $DATA ]]; then
+  printf "The environment variable 'ASSETS' or 'DATA' is not set"
+  exit 1
+fi
+
 BLOCK_STATES="${ASSETS}/blockstates"
 MODELS_BLOCK="${ASSETS}/models/block"
 MODELS_ITEM="${ASSETS}/models/item"
@@ -16,31 +21,38 @@ RECIPE_ADVANCEMENTS="${DATA}/advancements/recipes"
 [[ ! -d "${RECIPES}" ]] && mkdir -p "${RECIPES}"
 [[ ! -d "${RECIPE_ADVANCEMENTS}" ]] && mkdir -p "${RECIPE_ADVANCEMENTS}"
 
+STORAGE_TYPE='crate'
 VARIANTS=('acacia' 'birch' 'dark_oak' 'jungle' 'oak' 'spruce' 'crimson' 'warped')
 TIERS=('default' 'tier1' 'tier2' 'tier3')
 
 for VARIANT in "${VARIANTS[@]}"; do
+  printf " +---> json for %s with variant '%-8s' is generating\n" "$STORAGE_TYPE" "$VARIANT"
+
   export VARIANT
   export VARIANT_LOG
+
   if [[ "${VARIANT}" == "crimson" || "${VARIANT}" == "warped" ]]; then
     VARIANT_LOG="${VARIANT}_stem"
   else
     VARIANT_LOG="${VARIANT}_log"
   fi
+
   for TIER in "${TIERS[@]}"; do
     export TIER
     export PREVIOUS_TIER
-    file="${VARIANT}_crate_${TIER}"
-    envsubst <"${BASE}/presets/crate/blockstate.json" >"${BLOCK_STATES}/${file}.json"
 
-    envsubst <"${BASE}/presets/crate/model_block_${TIER}.json" >"${MODELS_BLOCK}/${file}.json"
-    envsubst <"${BASE}/presets/crate/model_item.json" >"${MODELS_ITEM}/${file}.json"
+    file="${VARIANT}_${STORAGE_TYPE}_${TIER}"
+
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/blockstate.json" >"${BLOCK_STATES}/${file}.json"
+
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/model_block_${TIER}.json" >"${MODELS_BLOCK}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/model_item.json" >"${MODELS_ITEM}/${file}.json"
 
     [[ ! -d "${RECIPES}/${VARIANT}" ]] && mkdir -p "${RECIPES}/${VARIANT}"
-    envsubst <"${BASE}/presets/crate/recipe_${TIER}.json" >"${RECIPES}/${VARIANT}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/recipe_${TIER}.json" >"${RECIPES}/${VARIANT}/${file}.json"
 
     [[ ! -d "${RECIPE_ADVANCEMENTS}/${VARIANT}" ]] && mkdir -p "${RECIPE_ADVANCEMENTS}/${VARIANT}"
-    envsubst < "${BASE}/presets/crate/advancement_${TIER}.json" > "${RECIPE_ADVANCEMENTS}/${VARIANT}/${file}.json"
+    envsubst <"${BASE}/presets/${STORAGE_TYPE}/advancement_${TIER}.json" >"${RECIPE_ADVANCEMENTS}/${VARIANT}/${file}.json"
 
     PREVIOUS_TIER="${TIER}"
   done
